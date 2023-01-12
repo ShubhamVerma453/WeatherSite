02065e6b42e8239f72c57c5fc2d8528a4279ad5f
ShubhamVerma453 <sv4537373@gmail.com>
2023-01-12 22:41:27 +0530

    rendering completed

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const port = process.env.port || 3000;
+
+const app = express();
+app.use(express.static("public"));   
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/html/index.html");
+});
+
+app.get("/about", (req, res)=>{
+    res.sendFile(__dirname+"/html/aboutMe.html");
+});
+
+app.get("/project", (req, res)=>{
+    res.sendFile(__dirname+"/html/otherWork.html");
+});
+
+app.listen(port, ()=>{
+    console.log("listening");
+});
