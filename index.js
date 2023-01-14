f26abbd51415b6012e6a45ef842442fd40ccf56b
ShubhamVerma453 <sv4537373@gmail.com>
2023-01-14 11:10:51 +0530

    api testing pass

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.get("/", (req, res)=>{
     res.sendFile(__dirname+"/html/index.html");
 });
 
+app.post("/", (req, res)=>{
+    let city = req.body.city;
+    let url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=c51fe6c18a02dda49ab9619742a109cd&units=metric";
+    https.get(url, (response)=>{
+        response.on("data", (data)=>{
+            let value = JSON.parse(data);
+            // console.log(value);
+            if(value.cod == 404){
+                console.log("ok");
+            }else{
+                console.log("not");
+            }
+        });
+        res.send(city);
+    });
+});
+
 app.get("/about", (req, res)=>{
     res.sendFile(__dirname+"/html/aboutMe.html");
 });
