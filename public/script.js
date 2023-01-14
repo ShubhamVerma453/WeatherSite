d02912ef3452486a53c9d1420d7363c8072f9f21
ShubhamVerma453 <sv4537373@gmail.com>
2023-01-14 23:06:43 +0530

    most errors fixed in api data

--- public/script.js
+++ public/script.js
@@ -5,19 +5,25 @@ function searchData(event) {
     event.preventDefault();
     // alert("ok");
     let city = document.getElementById("form-city").value;
-    let url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=c51fe6c18a02dda49ab9619742a109cd&units=metric";
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c51fe6c18a02dda49ab9619742a109cd&units=metric";
     try {
-        
         let data = fetch(url).then(response => response.json()).then(data => {
             let cod = data.cod;
-            if(cod == 404)
-                return;
-    
-            document.getElementById("city").innerText = data.name;
-            document.getElementById("country").innerText = data.sys.country;
-            document.getElementById("temp").innerText = data.main.temp;
-            let logo = data.weather[0].icon;
-            document.getElementById("weather-data").classList.remove("display-none");
+            if (cod == 200) {
+                document.getElementById("city").innerText = data.name;
+                document.getElementById("country").innerText = data.sys.country;
+                document.getElementById("temp").innerText = data.main.temp;
+                let logo = data.weather[0].icon;
+                let imgSrc = "http://openweathermap.org/img/wn/" + logo + "@2x.png";
+                document.getElementById("img").src = imgSrc;
+                document.getElementById("temp-box").classList.remove("display-none");
+                document.getElementById("weather-img").classList.remove("display-none");
+            } else {
+                document.getElementById("city").innerText = "city not found";
+                document.getElementById("temp-box").classList.add("display-none");
+                document.getElementById("weather-img").classList.add("display-none");
+            }
+
         });
     } catch (error) {
         console.log("error");
